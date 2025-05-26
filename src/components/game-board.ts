c6bf69dc3f55757ca422ff96a017f6bcf2e23dd5
robgon-art <robgon.art@gmail.com>
2025-05-26 13:41:52 -0400

    Add new card style and grid size sliders

--- src/components/game-board.ts
+++ src/components/game-board.ts
@@ -19,6 +19,8 @@ export type GameCompletionCallback = (moves: number) => void;
 export class GameBoard extends LitElement {
   @state() gameState: GameState;
   @state() private isVictoryMusicPlaying = false;
+  @state() private cardStyleValue = 0; // State for card style slider value
+  @state() private gridSizeValue = 0; // State for grid size slider value
 
   @property({ type: Object })
   timerService: TimerService = defaultTimerService;
@@ -267,8 +269,8 @@ export class GameBoard extends LitElement {
         </div>
         <memory-grid .numPairs=${this.gameState.cards.length / 2}>
           ${this.gameState.cards.map((card) => {
-            const props = pairAnimationProps.get(card.imageId);
-            return html`
+          const props = pairAnimationProps.get(card.imageId);
+          return html`
             <flip-card
               .frontImage=${this.getCardImagePath(card.imageId)}
               .backImage=${this.backImage}
@@ -283,7 +285,19 @@ export class GameBoard extends LitElement {
             ></flip-card>
           `})}
         </memory-grid>
-        <button @click=${this.restartGame} class="restart-button">Restart Game</button>
+        <div class="game-controls">
+          <div class="card-style-control">
+            <label for="cardStyleSlider" @click=${() => this.cardStyleValue = 0}>Impressionist</label>
+            <input type="range" id="cardStyleSlider" min="0" max="1" .value=${this.cardStyleValue} @input=${(e: Event) => this.cardStyleValue = parseInt((e.target as HTMLInputElement).value)}>
+            <label for="cardStyleSlider" @click=${() => this.cardStyleValue = 1}>RobGon</label>
+          </div>
+          <div class="grid-size-control">
+            <label for="gridSizeSlider" @click=${() => this.gridSizeValue = 0}>Easy</label>
+            <input type="range" id="gridSizeSlider" min="0" max="1" .value=${this.gridSizeValue} @input=${(e: Event) => this.gridSizeValue = parseInt((e.target as HTMLInputElement).value)}>
+            <label for="gridSizeSlider" @click=${() => this.gridSizeValue = 1}>Hard</label>
+          </div>
+          <button @click=${this.restartGame} class="restart-button">Restart Game</button>
+        </div>
       </div>
     `;
   }
